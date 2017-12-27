875e17ff5cdf0e18f9d36c6c321dd7bf82b04224
ar1st0crat <art1st.prog@gmail.com>
2017-12-27 22:52:17 +0300

    add first lecture (slides and code)

---
+++ Spring/Code/Lec01/EquationSolvingApplication/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace EquationSolvingApplication
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSolve_Click(object sender, EventArgs e)
+        {
+            var solver = new EquationSolver();
+
+            var roots = solver.Solve(double.Parse(textBoxA.Text),
+                                     double.Parse(textBoxB.Text),
+                                     double.Parse(textBoxC.Text));
+
+            textBoxRoot1.Text = roots[0].ToString("F3");
+            textBoxRoot2.Text = roots[1].ToString("F3");
+        }
+    }
+}
---
+++ Spring/Code/Lec01/EquationSolvingTestProject/EquationSolvingTestProject/UnitTest1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using EquationSolvingApplication;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EquationSolvingTestProject
+{
+    [TestClass]
+    public class QuadraticEquationUnitTest
+    {
+        readonly EquationSolver _solver = new EquationSolver();
+
+        [TestInitialize]
+        public void QuadraticEquationTestInitialize()
+        {
+            // additional test setup logic
+        }
+
+        [TestMethod]
+        public void TestTwoDifferentRoots()     // "AAA" : Triple A
+        {
+            // ACT
+            double[] roots = _solver.Solve(1, 1, -6);
+
+            // ASSERT
+            CollectionAssert.AreEquivalent(roots, new [] { 2.0, -3.0 });
+
+            // we could also test:
+
+            // 1) CollectionAssert.AllItemsAreNotNull(roots);
+            // 2) CollectionAssert.AllItemsAreUnique(roots);
+        }
+
+        [TestMethod]
+        public void TestOneRoot()
+        {
+            // ACT
+            double[] roots = _solver.Solve(1, 2, 1);
+
+            // ASSERT
+            Assert.AreEqual(roots[0], -1);
+        }
+
+        [TestMethod]
+        public void TestZeroCoefficients()
+        {
+            double[] roots = _solver.Solve(0, 1, 2);
+
+            // ASSERT
+            Assert.AreEqual(roots[0], -2);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(Exception))]
+        public void TestNoRealRoots()
+        {
+            // ARRANGE, ACT, ASSERT in one line
+            _solver.Solve(1, 1, 1);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void TestNoUnknowns()
+        {
+            // ARRANGE, ACT, ASSERT in one line
+            _solver.Solve(0, 0, 1);
+        }
+    }
+}
