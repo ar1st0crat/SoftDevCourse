d68c35fdc3a742bd74acc4e9abab2d7608e05b83
ar1st0crat <art1st.prog@gmail.com>
2018-01-02 00:12:37 +0300

    add lecture 04

---
+++ Spring/Code/Lec04/WebDriverBotApplication/WebDriverBotApplication/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Linq;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+namespace WebDriverBotApplication
+{
+    // Простой бот, который проверяет, что адрес donnu.ru 
+    // присутствует в первых результатах по запросу "donnu" 
+    // поисковиков: сначала Google, затем Yandex
+
+    class Program
+    {
+        static readonly IWebDriver _driver = new ChromeDriver();
+
+        static void Main()
+        {
+            const string addressToAppear = "donnu.ru";
+            
+            _driver.Navigate().GoToUrl(@"http://www.google.com");
+
+            IWebElement search = _driver.FindElement(By.Id("lst-ib"));
+
+            search.SendKeys("donnu");
+            System.Threading.Thread.Sleep(250);         // for visualization
+            search.SendKeys(Keys.Enter);
+
+            // Not a good way of waiting
+            // System.Threading.Thread.Sleep(1500);     // for waiting
+
+            WebDriverWait wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
+
+            wait.Until(d => d.FindElement(By.ClassName("_Rm")));
+
+
+            var entries = _driver.FindElements(By.ClassName("_Rm"));
+
+            if (entries.Any(entry => entry.Text.Contains(addressToAppear)))
+            {
+                Console.WriteLine("Google Test OK!");
+            }
+
+            _driver.Navigate().GoToUrl(@"http://ya.ru");
+
+            wait.Until(d => d.FindElement(By.Id("text")));
+
+            search = _driver.FindElement(By.Id("text"));
+            search.SendKeys("donnu");
+            System.Threading.Thread.Sleep(250);     // for visualization
+            search.SendKeys(Keys.Enter);
+
+            wait.Until(d => d.FindElement(By.ClassName("serp-list")));
+
+            try
+            {
+                entries = _driver.FindElements(By.XPath("//a[contains(@class, 'link')]/b"));
+
+                if (entries.Any(entry => entry.Text.Contains(addressToAppear)))
+                {
+                    Console.WriteLine("Yandex Test OK!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.GetType().Name + "\n" + ex.Message);
+            }
+            finally
+            {
+                _driver.Quit();
+            }
+        }
+    }
+}
---
+++ Spring/Code/Lec04/WebDriverTest/WebDriverTest/SeleniumTestClass.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace WebDriverTest
+{
+    [TestFixture]
+    public class SeleniumTestClass
+    {
+        private const string Keyword = "C#";
+
+        private IWebDriver _driver;
+
+        [OneTimeSetUp]
+        public void Init()
+        {
+            _driver = new ChromeDriver();
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+        }
+
+        [OneTimeTearDown]
+        public void Quit()
+        {
+            _driver.Quit();
+        }
+
+        [Test]
+        public void TestSearchResults()
+        {
+            var searchPage = new SeleniumSearchPage(_driver);
+            searchPage.Navigate();
+            
+            var resultsPage = searchPage.Search(Keyword);
+
+            Assert.That(resultsPage.Links, Does.Contain("Downloads"));
+        }
+    }
+}
